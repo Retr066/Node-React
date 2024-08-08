c7b252a5ee3a92926fe42f9f5e5e9cea1212852b
Jherson <jherson.lopez88@gmail.com>
2024-08-07 22:28:00 -0500

    Remove ignored files and directories

--- backend/dist/routes/articleRoute.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const articleController_1 = require("../controllers/articleController");
-const router = (0, express_1.Router)();
-router.get('/articles', articleController_1.getArticles);
-exports.default = router;
